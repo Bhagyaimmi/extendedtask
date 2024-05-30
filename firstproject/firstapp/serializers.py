c2022b056ff3648f48dfe199ea9ddb097a2ac15e
Bhagyaimmi <imbhagya30@gmail.com>
2024-05-30 03:59:53 -0500

    upadte status and priority of task managemnet

--- firstproject/firstapp/serializers.py
+++ firstproject/firstapp/serializers.py
@@ -12,18 +12,6 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         fields = ['id', 'title', 'content', 'author', 'product']
 
-# class UserSerializer(serializers.ModelSerializer):
-#     products = ProductSerializer(many=True, read_only=True)
-#     articles = ArticleSerializer(many=True, read_only=True)
-#     projects = ProjectSerializer(many=True, read_only=True)
-#     tasks = TaskSerializer(many=True, read_only=True)
-
-#     class Meta:
-#         model = User
-#         fields = ['id', 'username', 'email', 'products', 'articles','password', 'tasks','projects']
-        
-#     def create(self, validated_data):
-#          return User.objects.create(**validated_data)
 
 class UserSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
--- firstproject/firstapp/urls.py
+++ firstproject/firstapp/urls.py
@@ -1,28 +1,26 @@
 from django.urls import path
+from firstapp import views
 
-from .views import (
-    RegisterView,UserListView,UserDetailView, ProductListView, ProductDetailView,
-    ArticleListView, ArticleDetailView,
-    SampleEmailNotification,UserCSVExportView,UserExcelExportView,
-     ProductCSVExportView ,ProductExcelExportView, ArticleExcelExportView,ArticleCSVExportView,
-    
-)
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='create'),
-    path('users/', UserListView.as_view(), name='user-list'),
-    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('articles/', ArticleListView.as_view(), name='article-list'),
-    path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
-    path('sample/email/notification/', SampleEmailNotification.as_view(), name='sample-email'),
-    path('export/users/csv/', UserCSVExportView.as_view(), name='export-users-csv'),
-    path('export/users/excel/', UserExcelExportView.as_view(), name='export-users-excel'),
-    path('export/products/csv/', ProductCSVExportView.as_view(), name='export-products-csv'),
-    path('export/products/excel/', ProductExcelExportView.as_view(), name='export-products-excel'),
-    path('export/articles/csv/', ArticleCSVExportView.as_view(), name='export-articles-csv'),
-    path('export/articles/excel/', ArticleExcelExportView.as_view(), name='export-articles-excel'),
-  
+    path('register/',views.RegisterView.as_view(), name='register'),
+    path('users/', views.UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetailView.as_view(), name='user-detail'),
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('articles/', views.ArticleListView.as_view(), name='article-list'),
+    path('articles/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
+    path('sample/email/notification/', views.SampleEmailNotification.as_view(), name='sample-email'),
+    path('export/users/csv/', views.UserCSVExportView.as_view(), name='export-users-csv'),
+    path('export/users/excel/', views.UserExcelExportView.as_view(), name='export-users-excel'),
+    path('export/products/csv/', views.ProductCSVExportView.as_view(), name='export-products-csv'),
+    path('export/products/excel/', views.ProductExcelExportView.as_view(), name='export-products-excel'),
+    path('export/articles/csv/', views.ArticleCSVExportView.as_view(), name='export-articles-csv'),
+    path('export/articles/excel/', views.ArticleExcelExportView.as_view(), name='export-articles-excel'),
+    path('token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('password/reset/', views.PasswordResetView.as_view(), name='password-reset'),
+    path('password/reset/confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    
 ]
